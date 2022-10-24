abeff3a443b3de690a04e1c4957e7db28edb0a60
asayel <example.com>
2022-10-24 10:23:58 +0300

    Initial Commit

---
+++ TEST/TESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  TESTApp.swift
+//  TEST
+//
+//  Created by asayel on 28/03/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct TESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
